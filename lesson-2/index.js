136bb74eca3cbae037e9e3152cfa04d52cf31ced
Nihir <nihir.z@evolutionloud.in>
2025-08-27 17:19:35 +0530

    Lesson:2 - about modules

---
+++ lesson-2/index.js
@@ -0,0 +1,10 @@
+const { add, sub, mul, div, mod, pow, sqrt } = require('./module/math');
+
+
+console.log("Addition: ", add(1, 2));
+console.log("Subtraction: ", sub(1, 2));
+console.log("Multiplication: ", mul(1, 2));
+console.log("Division: ", div(1, 2));
+console.log("Modulus: ", mod(1, 2));
+console.log("Power: ", pow(1, 2));
+console.log("Square Root: ", sqrt(1));
---
+++ lesson-2/module/math.js
@@ -0,0 +1,37 @@
+function add(a, b) {
+    return a + b;
+}
+
+function sub(a, b) {
+    return a - b;
+}
+
+function mul(a, b) {
+    return a * b;
+}
+
+function div(a, b) {
+    return a / b;
+}
+
+function mod(a, b) {
+    return a % b;
+}
+
+function pow(a, b) {
+    return Math.pow(a, b);
+}
+
+function sqrt(a) {
+    return Math.sqrt(a);
+}
+
+module.exports = {
+    add,
+    sub,
+    mul,
+    div,
+    mod,
+    pow,
+    sqrt,
+}   
